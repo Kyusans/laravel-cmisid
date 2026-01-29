8a823ffd76cb23a8d5441582adcf1c1b50d2b2ce
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2026-01-29 10:28:14 +0800

    added tblsystemproblems for migration

---
+++ database/migrations/2026_01_29_022139_create_tblsystemproblems_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tblsystemproblems', function (Blueprint $table) {
+            $table->id("sysprob_id");
+            $table->foreignId("sysprob_infoSysId")
+                ->constrained("tblinformationsystems", "infoSys_id")
+                ->onDelete("cascade");
+            $table->string("sysprob_name");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tblsystemproblems');
+    }
+};
