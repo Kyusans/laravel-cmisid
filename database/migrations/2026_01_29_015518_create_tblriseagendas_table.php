f702186c12764588935fca415d46dab29f8267d6
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2026-01-29 10:01:55 +0800

    added migration for tblriseagendas

---
+++ database/migrations/2026_01_29_015518_create_tblriseagendas_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tblriseagendas', function (Blueprint $table) {
+            $table->id("rise_id");
+            $table->string("rise_name")->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tblriseagendas');
+    }
+};
