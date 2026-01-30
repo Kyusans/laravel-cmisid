ff213a1e2cdd6a608f7315f44a7bde4be71db335
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2026-01-30 15:59:53 +0800

    updated login form into floating label

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -5,23 +5,25 @@
     <div class="container">
       <form id="loginForm">
         @csrf
-        <div class="form-group mb-3">
-          <label for="email">Email address</label>
-          <input type="email" class="form-control" id="login_email" aria-describedby="emailHelp"
-            placeholder="Enter email">
+
+        <div class="form-floating mb-3">
+          <input type="email" class="form-control" id="login_email" placeholder="name@example.com" required>
+          <label for="login_email">Email address</label>
         </div>
-        <div class="form-group">
+        <div class="form-floating">
+          <input type="password" class="form-control" id="login_password" placeholder="Password" required>
           <label for="login_password">Password</label>
-          <input type="password" class="form-control" id="login_password" placeholder="Password">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+
+        <div class="d-flex justify-content-center align-items-center">
+          <button type="submit" class="btn btn-outline-dark mt-3">Submit</button>
+        </div>
       </form>
     </div>
   </div>
 
   <script>
     const loginForm = document.getElementById("loginForm");
-
     loginForm.addEventListener('submit', async (e) => {
       e.preventDefault();
       const url = "/api/user/login"
@@ -38,7 +40,6 @@
       })
       const res = await response.json();
       console.log("res", res);
-
     })
   </script>
 @endsection
