a7fd17aae1efc0889545ec01fd7a654ef1f03620
Faisal Tamano <faisalrichardjr@gmail.com>
2026-02-10 00:39:54 +0800

    restructured the views and added new login and registration page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,7 +10,7 @@ class AuthController extends Controller
 {
     public function index()
     {
-        return view('pages.login');
+        return view('auth.login');
     }
 
     public function login(Request $request)
