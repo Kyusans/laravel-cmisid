5fbc61b401767eca8d99ac47f5f79d1f7845eba6
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2026-02-11 14:47:43 +0800

    added UserMasterController.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,16 @@
 <?php
 
 use App\Http\Controllers\Masterfiles\OfficeMasterController;
+use App\Http\Controllers\Masterfiles\UserMasterController;
 use Illuminate\Support\Facades\Route;
 
+
 // office masterfile
+Route::get("/office-master", [OfficeMasterController::class, "index"])->name("office-master");
 Route::post("/add-office", [OfficeMasterController::class, "addOffice"])->name("add-office");
+Route::post("/update-office", [OfficeMasterController::class, "updateOffice"])->name("update-office");
+
+// user masterfile
+Route::get("/user-master", [UserMasterController::class, "index"])->name("user-master");
+Route::post("/add-user", [UserMasterController::class, "addUser"])->name("add-user");
+Route::post("/update-user", [UserMasterController::class, "updateUser"])->name("update-user");
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Masterfiles\OfficeMasterController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, "index"])->name("/");
@@ -13,9 +14,3 @@ Route::get("/dashboard", [DashboardController::class, "index"])->name("dashboard
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
