12e1dd2c953afcd7716ee788cf0869c6aae6d850
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2026-02-13 14:12:22 +0800

    added livewire

--- app/Models/SystemType.php
+++ app/Models/SystemType.php
@@ -9,10 +9,8 @@ class SystemType extends Model
     protected $table = 'tblsystemtypes';
     protected $primaryKey = "systemType_id";
 
-    protected $fillable =[
-    "systemType_name",
-    "systemType_description"
+    protected $fillable = [
+        "systemType_name",
+        "systemType_description"
     ];
-
-    
 }
